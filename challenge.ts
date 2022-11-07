f7d130963162d7a13d0da908f40a724295f3c8a4
ngroult <nolwenn.groult@gmail.com>
2022-11-07 10:58:31 +0100

    challenge.ts modified

--- challenge.ts
+++ challenge.ts
@@ -1,14 +1,20 @@
 // challenge.ts
-const prettyPrintWilder = (users) => {
-    users.map((user) => {
+export interface User {
+    name : string;
+    age?: number;
+    birthday?: string;
+}
+
+const prettyPrintWilder = (users: User[]) : void => {
+    users.map((user: User) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
   
-  const wilders = [];
-  const user1 = { name: "Pierre", age: 23 };
-  const user2 = { name: "Paul", birthday: "10/02/1990" };
-  const user3 = { name: "Jacques", age: 25 };
+  let wilders: User[] = [];
+  const user1: User = { name: "Pierre", age: 23 };
+  const user2: User = { name: "Paul", birthday: "10/02/1990" };
+  const user3: User = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
   wilders.push(user3);
