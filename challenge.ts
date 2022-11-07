e82d811f10b0fbccda5dadddc6c7280e89032009
ngroult <nolwenn.groult@gmail.com>
2022-11-07 10:11:03 +0100

    add challenge.ts

---
+++ challenge.ts
@@ -0,0 +1,16 @@
+// challenge.ts
+const prettyPrintWilder = (users) => {
+    users.map((user) => {
+      console.log(`${user.name} is ${user.age} years old`);
+    });
+  };
+  
+  const wilders = [];
+  const user1 = { name: "Pierre", age: 23 };
+  const user2 = { name: "Paul", birthday: "10/02/1990" };
+  const user3 = { name: "Jacques", age: 25 };
+  wilders.push(user1);
+  wilders.push(user2);
+  wilders.push(user3);
+  
+  prettyPrintWilder(wilders);
